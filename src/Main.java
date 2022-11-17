799cc0ad14506cceeebcc5d8afaea723207acc00
salva <salvationordeath@gmail.com>
2022-11-17 20:56:47 +0700

    ДЗ 8 - задачи с 1 по 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("ДЗ 1:");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("ФИО сотрудника — " + fullName + ".");
+
+        System.out.println("ДЗ 2:");
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        System.out.println("ДЗ 3:");
+        String fullName1 = "Иванов Семён Семёнович";
+        System.out.println("Данные ФИО сотрудника — " + fullName1.replace('ё', 'e'));
     }
 }
